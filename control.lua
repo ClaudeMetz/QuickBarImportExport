9897ac8a614498366989ffa1b63982ecfc4cda7a
Claude Metz <claudearmetz@gmail.com>
2020-11-25 20:48:29 +0100

    Modernize interface somewhat (still crappy)

--- control.lua
+++ control.lua
@@ -32,6 +32,13 @@ script.on_event("qbie_export", function(event)
 end)
 
 
+script.on_event(defines.events.on_gui_confirmed, function(event)
+    local player = game.get_player(event.player_index)
+    if event.element.name == "qbie_textfield_quickbar_string" then
+        close_main_window(player, "submit")
+    end
+end)
+
 script.on_event(defines.events.on_lua_shortcut, function(event)
     local player = game.get_player(event.player_index)
     if event.prototype_name == "qbie-import-quickbar" then
@@ -45,7 +52,7 @@ end)
 script.on_event(defines.events.on_gui_click, function(event)
     local player = game.get_player(event.player_index)
 
-    if event.element.type == "text-box" then
+    if event.element.type == "textfield" then
         event.element.select_all()
 
     elseif event.element.name == "qbie_button_show_options" then
@@ -128,45 +135,50 @@ function create_main_window(player, type)
     hide_actions(player)
 
     local window = mod_gui.get_frame_flow(player).add{type="frame", name="qbie_frame_main_window",
-      direction="vertical", style="inner_frame_in_outer_frame", caption={"label." .. type}}
+      direction="vertical", caption={"label." .. type}}
 
-    local label_warning = window.add{type="label", name="qbie_label_warning", caption={"label." .. type .. "_warning"},
+    local content_frame = window.add{type="frame", name="content_frame", direction="vertical",
+      style="inside_shallow_frame_with_padding"}
+
+    -- Labels and textbox
+    content_frame.add{type="label", name="qbie_label_warning", caption={"label." .. type .. "_warning"},
       tooltip={"label." .. type .. "_warning_tooltip"}}
-    label_warning.style.single_line = false
 
-    local label_error = window.add{type="label", name="qbie_label_error_message", caption=""}
+    local label_error = content_frame.add{type="label", name="qbie_label_error_message", caption=""}
     label_error.style.font_color = {r = 1, g = 0.2, b = 0.2}
-    label_error.style.single_line = false
     label_error.visible = false
 
-    local text_box = window.add{type="text-box", name="qbie_text-box_quickbar_string"}
-    text_box.style.height = 40
-    text_box.style.width = 400
-    text_box.style.top_margin = 4
-    text_box.style.bottom_margin = 6
+    local textfield = content_frame.add{type="textfield", name="qbie_textfield_quickbar_string"}
+    textfield.style.width = 400
+    textfield.style.top_margin = 10
 
     if type == "export" then
-        text_box.text = generate_export_string(player)
-        text_box.select_all()
+        textfield.text = generate_export_string(player)
+        textfield.select_all()
     end
-    text_box.focus()
+    textfield.focus()
 
-    local button_bar = window.add{type="flow", name="qbite_flow_button_bar", direction="horizontal"}
-    button_bar.add{type="button", name="qbie_button_close_window", caption={"label.close"}}
-    local spacer = button_bar.add{type="flow", name="qbie_flow_spacer", direction="horizontal"}
-    spacer.style.horizontally_stretchable = true
+    -- Button bar
+    local button_bar = window.add{type="flow", direction="horizontal", style="dialog_buttons_horizontal_flow"}
+
+    button_bar.add{type="button", name="qbie_button_close_window", style="back_button", caption={"label.close"}}
+
+    local pusher = button_bar.add{type="empty-widget", style="draggable_space"}
+    pusher.style.horizontally_stretchable = true
+    pusher.style.height = 32
+    pusher.style.margin = {0, 4}
 
     if type == "import" then
-        button_bar.add{type="button", name="qbie_button_submit_window", caption={"label.submit"}}
+        button_bar.add{type="button", name="qbie_button_submit_window", style="confirm_button", caption={"label.submit"}}
     end
 end
 
 function close_main_window(player, action)
-    local window = mod_gui.get_frame_flow(player)["qbie_frame_main_window"]
+    local content_frame = mod_gui.get_frame_flow(player)["qbie_frame_main_window"]["content_frame"]
 
     local error_message
     if action == "submit" then
-        local encoded_string = window["qbie_text-box_quickbar_string"].text
+        local encoded_string = content_frame["qbie_textfield_quickbar_string"].text
         if encoded_string == "" then
             error_message = {"label.error_invalid_string"}
         else
@@ -182,17 +194,17 @@ function close_main_window(player, action)
         end
 
         if error_message ~= nil then
-            local error_message_label = window["qbie_label_error_message"]
+            local error_message_label = content_frame["qbie_label_error_message"]
             error_message_label.visible = true
             error_message_label.caption = error_message
-            window["qbie_label_warning"].visible = false
+            content_frame["qbie_label_warning"].visible = false
 
-            window["qbie_text-box_quickbar_string"].focus()
+            content_frame["qbie_textfield_quickbar_string"].focus()
             return  -- Early return so the window doesn't close
         end
     end
 
-    window.destroy()
+    content_frame.parent.destroy()
 end
 
 
